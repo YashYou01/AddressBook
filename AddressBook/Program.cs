dac9f33ce182d414973695df079031ec4f2fba55
YashYou01 <yashugarg012003@gmail.com>
2025-12-31 12:36:10 +0530

    UC1 completed

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -1,10 +1,44 @@
-﻿namespace AddressBook
+﻿using System;
+using AddressBook.Models;
+using AddressBook.Services;
+
+namespace AddressBook
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to Address Book Program");
+
+            AddressBookService addressBook = new AddressBookService();
+            ContactPerson person = new ContactPerson();
+
+            Console.Write("Enter First Name: ");
+            person.FirstName = Console.ReadLine();
+
+            Console.Write("Enter Last Name: ");
+            person.LastName = Console.ReadLine();
+
+            Console.Write("Enter Address: ");
+            person.Address = Console.ReadLine();
+
+            Console.Write("Enter City: ");
+            person.City = Console.ReadLine();
+
+            Console.Write("Enter State: ");
+            person.State = Console.ReadLine();
+
+            Console.Write("Enter Zip: ");
+            person.Zip = Console.ReadLine();
+
+            Console.Write("Enter Phone Number: ");
+            person.PhoneNumber = Console.ReadLine();
+
+            Console.Write("Enter Email: ");
+            person.Email = Console.ReadLine();
+
+            addressBook.AddContact(person);
+            addressBook.DisplayContact();
         }
     }
 }
---
+++ AddressBook/Services/AddressBookService.cs
@@ -0,0 +1,30 @@
+﻿using AddressBook.Models;
+using System;
+
+namespace AddressBook.Services
+{
+    public class AddressBookService
+    {
+        private ContactPerson person;
+
+        // UC-1: Add Contact
+        public void AddContact(ContactPerson contact)
+        {
+            person = contact;
+            Console.WriteLine("\nContact added successfully!\n");
+        }
+
+        public void DisplayContact()
+        {
+            Console.WriteLine("Contact Details:");
+            Console.WriteLine("First Name : " + person.FirstName);
+            Console.WriteLine("Last Name  : " + person.LastName);
+            Console.WriteLine("Address    : " + person.Address);
+            Console.WriteLine("City       : " + person.City);
+            Console.WriteLine("State      : " + person.State);
+            Console.WriteLine("Zip        : " + person.Zip);
+            Console.WriteLine("Phone No   : " + person.PhoneNumber);
+            Console.WriteLine("Email      : " + person.Email);
+        }
+    }
+}
